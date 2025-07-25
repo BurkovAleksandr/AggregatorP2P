2db50352f94a537a60f9b95662c635908fd9fe21
Aleksandr <sanek00000007@gmail.com>
2025-07-25 15:52:37 +0300

    Удалил фронт с прошлого проекта так как слишком много передлывать

--- front/user/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from .views import auth_view, register_view
-
-urlpatterns = [
-    path("auth/", auth_view, name="auth"),
-    path("register/", register_view, name="register"),
-]
